fe99498632bb9a0086b0ba429e30510a91ae5387
duanfy <1694265627@qq.com>
2020-11-02 22:02:55 +0800

    首页

---
+++ consumer/src/main/java/com/jk/controller/PagejumpController.java
@@ -0,0 +1,12 @@
+package com.jk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PagejumpController {
+    @RequestMapping("index")
+    public String index(){
+        return "index";
+    }
+}
