91fb1b9ba857eb01031d9bd482e85cadf45024f3
fengshiqiang11 <1547475697@qq.com>
2020-11-05 17:15:30 +0800

    精选

---
+++ common/src/main/java/com/jk/entity/YcOriginal.java
@@ -0,0 +1,33 @@
+package com.jk.entity;
+
+public class YcOriginal {
+    private Integer originalId;
+
+    private String originalAuthor;
+
+    private String originalUrl;
+
+    public Integer getOriginalId() {
+        return originalId;
+    }
+
+    public void setOriginalId(Integer originalId) {
+        this.originalId = originalId;
+    }
+
+    public String getOriginalAuthor() {
+        return originalAuthor;
+    }
+
+    public void setOriginalAuthor(String originalAuthor) {
+        this.originalAuthor = originalAuthor == null ? null : originalAuthor.trim();
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl == null ? null : originalUrl.trim();
+    }
+}
